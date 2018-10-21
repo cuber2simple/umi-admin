e3aa766b8abc67564cf3b0814761c0d498547049
satan31415 <471867900@qq.com>
2018-10-21 16:30:11 +0800

    完善样式和细节

--- .umirc.js
+++ .umirc.js
@@ -15,7 +15,7 @@ export default {
       routes: {
         exclude: [],
       },
-      hardSource: true,
+      hardSource: false,
     }],
   ],
   proxy: {
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -47,25 +47,27 @@ class Index extends Component {
     const { submitLoading } = this.props;
     const antIcon = <Icon type="loading" style={{ fontSize: 24 }} spin/>;
     return (
-      <div className={styles['login-form']}>
-        <div className={styles['close']}/>
-        <div className={styles['head-info']}>
-          <label className={styles['lbl-1']}/>
-          <label className={styles['lbl-2']}/>
-          <label className={styles['lbl-3']}/>
-        </div>
-        <div className={styles['clear']}/>
-        <div className={styles['avtar']}>
-          <img src={avatar}/>
-        </div>
-        <form>
-          <input ref='username' type="text" className={styles['text']} placeholder={'username'} defaultValue='admin'/>
-          <input ref='password' type="password" placeholder={'password'} defaultValue='123456'/>
-        </form>
-        <div className={styles['signin']}>
-          <Spin indicator={antIcon} spinning={submitLoading === true}>
-            <input onClick={this.eventSubmit} type="submit" value={submitLoading === true ? '正在登陆...' : '登陆'}/>
-          </Spin>
+      <div className={styles['login']}>
+        <div className={styles['login-form']}>
+          <div className={styles['close']}/>
+          <div className={styles['head-info']}>
+            <label className={styles['lbl-1']}/>
+            <label className={styles['lbl-2']}/>
+            <label className={styles['lbl-3']}/>
+          </div>
+          <div className={styles['clear']}/>
+          <div className={styles['avtar']}>
+            <img src={avatar}/>
+          </div>
+          <form>
+            <input ref='username' type="text" className={styles['text']} placeholder={'username'} defaultValue='admin'/>
+            <input ref='password' type="password" placeholder={'password'} defaultValue='123456'/>
+          </form>
+          <div className={styles['signin']}>
+            <Spin indicator={antIcon} spinning={submitLoading === true}>
+              <input onClick={this.eventSubmit} type="submit" value={submitLoading === true ? '正在登陆...' : '登陆'}/>
+            </Spin>
+          </div>
         </div>
       </div>
     );
